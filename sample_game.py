d1cd36e60a74ed778dbbc673e063a2c5132c6bb0
JayDBee <79239855+JayDBee@users.noreply.github.com>
2021-03-27 21:58:18 -0700

    Added different screens

--- sample_game.py
+++ sample_game.py
@@ -1,7 +1,9 @@
 # Example pygame program
 
-# Import and initialize character classes
+# Import and initialize pygame and sys libraries
+# Import characters file
 import pygame
+import sys
 import characters
 
 # import pygame.locals or easier access to key coordinates
@@ -12,10 +14,8 @@ from pygame.locals import (
     K_LEFT,
     K_RIGHT,
     K_ESCAPE,
+    MOUSEBUTTONDOWN,
 )
-
-start_ticks = 0
-total_ticks = 0
 
 # Icons
 icon = pygame.image.load('ninja.png')
@@ -31,7 +31,6 @@ flag = pygame.transform.scale(flag, (characters.MAX_SPRITE_SZ, characters.MAX_SP
 mainChar = characters.Sprite(characters.DEFAULT_X, characters.DEFAULT_Y, 0, 0, ninja)
 obstacle = characters.Sprite(characters.MAX_SCREEN_SZ / 2, 0, 0, 0, flag)
 
-
 pygame.init()
 
 # Set up the screen
@@ -42,55 +41,98 @@ def render(sprite):
     screen.blit(sprite.image, (sprite.x, sprite.y))
 
 
-# start of game timer
-start_ticks = pygame.time.get_ticks()
+''''
+----------------------------------------loops for menu screens-----------------------------
+'''
 
-# Run until the user asks to quit
-running = True
-while running:
 
-    # Fill the background with white
-    screen.fill((255, 255, 255))
+def start():
+    while True:
+        # Fill the background with white
+        screen.fill((0, 0, 0))
 
-    # Did the user click the window close button?
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+        click = False
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+            if event.type == MOUSEBUTTONDOWN:
+                if event.button == 1:
+                    click = True
 
-    # Track Key presses
-    if event.type == pygame.KEYDOWN:
-        if event.key == K_RIGHT:
-            mainChar.changed_x = .6
-        elif event.key == K_LEFT:
-            mainChar.changed_x = -.6
-        elif event.key == K_UP:
-            mainChar.changed_y = -.6
-        elif event.key == K_DOWN:
-            mainChar.changed_y = .6
-        elif event.key == K_ESCAPE:
-            total_ticks = pygame.time.get_ticks()
-            print("Total time: %2d" % (total_ticks - start_ticks))
-            running = False
+        mx, my = pygame.mouse.get_pos()
 
-    # Key lifts
-    if event.type == pygame.KEYUP:
-        if event.key == K_RIGHT or event.key == K_LEFT:
-            mainChar.changed_x = 0
-        elif event.key == K_UP or event.key == K_DOWN:
-            mainChar.changed_y = 0
+        start_button = pygame.Rect(50, 100, 200, 50)
+        exit_button = pygame.Rect(50, 200, 200, 50)
+        if start_button.collidepoint((mx, my)):
+            if click:
+                main()
+        if exit_button.collidepoint((mx, my)):
+            if click:
+                pygame.quit()
 
-    if mainChar.collided(obstacle):
-        print("Finished!")
+        pygame.draw.rect(screen, (250, 250, 250), start_button)
+        pygame.draw.rect(screen, (250, 250, 250), exit_button)
 
-    render(mainChar)
-    render(obstacle)
-    mainChar.enforce_bounds()
-    mainChar.update_sprite()
+        pygame.display.update()
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(screen, (0, 0, 255), (characters.MAX_SCREEN_SZ / 2, characters.MAX_SCREEN_SZ / 2), 75)
 
-    pygame.display.update()
+def main():
+    # start of game timer
+    start_ticks = pygame.time.get_ticks()
 
-# Done! Time to quit
-pygame.quit()
+    # Run until the user asks to quit
+    running = True
+    while running:
+
+        # Fill the background with white
+        screen.fill((255, 255, 255))
+
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            # Track Key presses
+            if event.type == pygame.KEYDOWN:
+                if event.key == K_RIGHT:
+                    mainChar.changed_x = .6
+                elif event.key == K_LEFT:
+                    mainChar.changed_x = -.6
+                elif event.key == K_UP:
+                    mainChar.changed_y = -.6
+                elif event.key == K_DOWN:
+                    mainChar.changed_y = .6
+                elif event.key == K_ESCAPE:
+                    total_ticks = pygame.time.get_ticks()
+                    print("Total time: %2d" % (total_ticks - start_ticks))
+                    running = False
+
+            # Key lifts
+            if event.type == pygame.KEYUP:
+                if event.key == K_RIGHT or event.key == K_LEFT:
+                    mainChar.changed_x = 0
+                elif event.key == K_UP or event.key == K_DOWN:
+                    mainChar.changed_y = 0
+
+        if mainChar.collided(obstacle):
+            print("Finished!")
+
+        render(mainChar)
+        render(obstacle)
+        mainChar.enforce_bounds()
+        mainChar.update_sprite()
+
+        # Draw a solid blue circle in the center
+        pygame.draw.circle(screen, (0, 0, 255), (characters.MAX_SCREEN_SZ / 2, characters.MAX_SCREEN_SZ / 2), 75)
+
+        pygame.display.update()
+
+
+# def game_over():
+
+''''
+----------------------------------------loops for menu screens-----------------------------
+'''
+
+start()
